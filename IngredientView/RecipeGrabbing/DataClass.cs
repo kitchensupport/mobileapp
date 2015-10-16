503d5a5b7a08aa1ac72ae29f9b775ec4a23d2679
Graham Park <park580@purdue.edu>
2015-10-16 01:17:49 -0400

    Missed some bits i guess

--- IngredientView/RecipeGrabbing/DataClass.cs
+++ IngredientView/RecipeGrabbing/DataClass.cs
@@ -4,6 +4,9 @@ namespace RecipeGrabbing
 {
 	public class DataClass
 	{
+		//Data is pretty much a container for the recipes
+		public System.Collections.ObjectModel.Collection<Recipe> matches { get; set; }
+		public string cat = "meow";
 		public DataClass ()
 		{
 		}
--- IngredientView/RecipeGrabbing/ResponseClass.cs
+++ IngredientView/RecipeGrabbing/ResponseClass.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections;
 
 namespace RecipeGrabbing
 {
 	public class ResponseClass
 	{
+		//For names to be updated, they MUST match the names found in the JSON
+		public string status { get; set; }
+		public DataClass data { get; set; } //Contains the recipes
 		public ResponseClass ()
 		{
 		}
