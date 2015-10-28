7795371d386d8c05463cdc45d1525d88a6a89b84
masonherhusky <mason.herhusky@gmail.com>
2015-10-28 17:53:50 -0400

    Started Implementing Login Screen
    Mimicked XAMARIN’s example of a login screen using forms

--- KitchenSupportWithForms/KitchenSupport/LoginPage.xaml.cs
+++ KitchenSupportWithForms/KitchenSupport/LoginPage.xaml.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
 
 using Xamarin.Forms;
 
 namespace KitchenSupport
 {
-	public partial class LoginPage : ContentPage
+	public partial class LoginPage : TabbedPage //, INotifyPropertyChanged
 	{
 		public LoginPage ()
 		{
+			/*this.LoginCommand = new Command<string>((key) =>
+				{
+					//load the main page
+				});*/
 			InitializeComponent ();
 		}
-		int count = 0;
-		public void OnButtonClicked(object sender, EventArgs args)
+
+		/*public void OnButtonClicked(object sender, EventArgs args)
 		{
 			count++;
 
 			((Button)sender).Text = 
 				String.Format("{0} click{1}!", count, count == 1 ? "" : "s");
 		}
+		*/
+	
+		//public ICommand LoginCommand { protected set; get; }
 	}
 }
 
