c9196373673d2d1a1080c44351b503e4c23aea64
masonherhusky <mason.herhusky@gmail.com>
2015-10-28 16:50:35 -0400

    Switched to Tamarin Forms
    Got rid of all old code, switched to Xamarin forms

---
+++ KitchenSupportWithForms/KitchenSupport/LoginPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace KitchenSupport
+{
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+		int count = 0;
+		public void OnButtonClicked(object sender, EventArgs args)
+		{
+			count++;
+
+			((Button)sender).Text = 
+				String.Format("{0} click{1}!", count, count == 1 ? "" : "s");
+		}
+	}
+}
+
