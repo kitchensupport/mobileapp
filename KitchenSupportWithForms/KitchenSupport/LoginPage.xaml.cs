ee2bdfcce2cf0a28426790e493334eb3b470c23f
masonherhusky <mason.herhusky@gmail.com>
2015-10-28 20:52:58 -0400

    Started adding logic to login using http request

--- KitchenSupportWithForms/KitchenSupport/LoginPage.xaml.cs
+++ KitchenSupportWithForms/KitchenSupport/LoginPage.xaml.cs
@@ -11,10 +11,10 @@ namespace KitchenSupport
 	{
 		public LoginPage ()
 		{
-			/*this.LoginCommand = new Command<string>((key) =>
+			this.LoginCommand = new Command<string>((key) =>
 				{
-					//load the main page
-				});*/
+					login(username.Text,password.Text);
+				});
 			InitializeComponent ();
 		}
 
@@ -27,7 +27,19 @@ namespace KitchenSupport
 		}
 		*/
 	
-		//public ICommand LoginCommand { protected set; get; }
+		public ICommand LoginCommand { protected set; get; }
+
+		private void login(string username, string password){
+
+			var client = new System.Net.Http.HttpClient ();
+			client.BaseAddress = new Uri("http://api.kitchen.support/accounts/login");
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Accept.Add (new MediaTypeWithQualityHeaderValue ("application/json"));
+			var response = client.PostAsync("{\n    \"email\": \"" + email + "\",\n    \"password\": \"" + password + "\"\n}");
+			if (response.Status == 200) {
+				//success
+			}
+		}
 	}
 }
 
