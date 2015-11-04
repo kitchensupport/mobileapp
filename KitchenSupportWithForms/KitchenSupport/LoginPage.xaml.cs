f47f8891972137c534a49fd370d05e6e81d9a927
masonherhusky <mason.herhusky@gmail.com>
2015-11-04 12:06:19 -0500

    Added more custom ui to login screen

--- KitchenSupportWithForms/KitchenSupport/LoginPage.xaml.cs
+++ KitchenSupportWithForms/KitchenSupport/LoginPage.xaml.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows.Input;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 using Xamarin.Forms;
 
 namespace KitchenSupport
 {
-	public partial class LoginPage : TabbedPage //, INotifyPropertyChanged
+	public partial class LoginPage : ContentPage //, INotifyPropertyChanged
 	{
 		public LoginPage ()
 		{
-			this.LoginCommand = new Command<string>((key) =>
+			/*this.LoginCommand = new Command<string>((key) =>
 				{
-					login(username.Text,password.Text);
-				});
+					login(email.Text,password.Text);
+				});*/
 			InitializeComponent ();
 		}
 
@@ -27,18 +30,36 @@ namespace KitchenSupport
 		}
 		*/
 	
-		public ICommand LoginCommand { protected set; get; }
+		//public ICommand LoginCommand { protected set; get; }
 
-		private void login(string username, string password){
-
+		private void login(object sender, EventArgs args){
 			var client = new System.Net.Http.HttpClient ();
-			client.BaseAddress = new Uri("http://api.kitchen.support/accounts/login");
+			string url = "http://api.kitchen.support/accounts/login";
+			string data = "{\n    \"email\": \"" + email.Text + "\",\n    \"password\": \"" + password.Text + "\"\n}";
+			var httpContent = new StringContent (data);
+			client.BaseAddress = new Uri(url);
 			client.DefaultRequestHeaders.Accept.Clear();
 			client.DefaultRequestHeaders.Accept.Add (new MediaTypeWithQualityHeaderValue ("application/json"));
-			var response = client.PostAsync("{\n    \"email\": \"" + email + "\",\n    \"password\": \"" + password + "\"\n}");
-			if (response.Status == 200) {
-				//success
+			var response = client.PostAsync(new Uri(url), httpContent);
+			//var message = response.Result.Content.ReadAsStringAsync();
+			if (response.Result.StatusCode.ToString() == "Sucess") {
+				ContentPage HomePage = new ContentPage ();
+				Navigation.PushModalAsync (HomePage);
 			}
+			else {
+				throw new Exception ("Bad response from server loggin in.");
+			}
+		}
+
+		private void register(object sender, EventArgs args){
+			ContentPage RegisterPage = new ContentPage();
+			Navigation.PushModalAsync (RegisterPage);
+		}
+
+		private void forgotPassword(object sender, EventArgs args){
+			//ContentPage ResetPage = new ContentPage();
+			Navigation.PushModalAsync (new ResetPage());
+
 		}
 	}
 }
