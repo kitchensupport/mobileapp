b8eac647d922dba7e5160f3dc94d7b52b40439f5
dragonice81 <garrett.lewis1@gmail.com>
2015-11-11 19:51:06 -0500

    Everything in one app

---
+++ KitchenSupportWithForms/KitchenSupport/HomePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using IngrediantViewXam;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace KitchenSupport
+{
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			InitializeComponent ();
+		}
+
+		private void findRecipes(object sender, EventArgs args){
+			Navigation.PushModalAsync (new FindRecipesPage());
+		}
+
+		private void viewSavedRecipes(object sender, EventArgs args){
+			Navigation.PushModalAsync (new SavedRecipesPage());
+		}
+
+		private void navigateToIngredientsView(object sender, EventArgs args){
+			Navigation.PushModalAsync (new Ingredients());
+		}
+	}
+}
+
---
+++ KitchenSupportWithForms/KitchenSupport/ResetPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace KitchenSupport
+{
+	public partial class ResetPage : ContentPage
+	{
+		public ResetPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
