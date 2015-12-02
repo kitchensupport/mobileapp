90f658e886bafe49b29dec0407dfa16761cffd25
darrell1994 <darrell_authentic@yahoo.com>
2015-12-02 01:05:38 -0500

    Create localData_iOS.cs

---
+++ KitchenSupportWithForms/iOS/localData_iOS.cs
@@ -0,0 +1,25 @@
+using System;
+using Foundation;
+
+[assembly: Xamarin.Forms.Dependency (typeof (KitchenSupport.iOS.localData_iOS))]
+namespace KitchenSupport.iOS
+{
+	public class localData_iOS: localDataInterface
+	{
+		public localData_iOS ()
+		{
+		}
+
+		public void save(string key, string value){
+			var prefs = NSUserDefaults.StandardUserDefaults;
+			//save user's properties for a specific keys
+			prefs.SetValueForKey (new NSString (value), new NSString (key));
+		}
+
+		public string load(string key){
+			var prefs = NSUserDefaults.StandardUserDefaults;
+			return prefs.StringForKey (key);
+		}
+	}
+}
+
