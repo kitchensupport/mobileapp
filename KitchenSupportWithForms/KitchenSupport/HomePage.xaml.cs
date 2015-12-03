1dd761db541052bb0626bafdcdc658ee08fa9fab
dragonice81 <garrett.lewis1@gmail.com>
2015-12-02 22:52:00 -0500

    Added Mason's back buttons

--- KitchenSupportWithForms/KitchenSupport/HomePage.xaml.cs
+++ KitchenSupportWithForms/KitchenSupport/HomePage.xaml.cs
@@ -1,28 +1,35 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace KitchenSupport
 {
-	public partial class HomePage : ContentPage
-	{
-		public HomePage ()
-		{
-			InitializeComponent ();
-		}
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
 
-		private void findRecipes(object sender, EventArgs args){
-			Navigation.PushModalAsync (new RecipeStream());
-		}
+        private void findRecipes(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new NavigationPage(new FindRecipesPage()));
+        }
 
-		private void viewSavedRecipes(object sender, EventArgs args){
-			Navigation.PushModalAsync (new SavedRecipesPage());
-		}
+        private void viewSavedRecipes(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new NavigationPage(new SavedRecipesPage()));
+        }
 
-		private void navigateToIngredientsView(object sender, EventArgs args){
-			Navigation.PushModalAsync (new Ingredients());
-		}
-	}
+        private void navigateToIngredientsView(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new NavigationPage(new Ingredients()));
+        }
+
+        private void navigateBack(object sender, EventArgs args)
+        {
+            //Navigation.PushModalAsync(new NavigationPage(new LoginPage()));
+            Navigation.PopModalAsync();
+        }
+    }
 }
-
